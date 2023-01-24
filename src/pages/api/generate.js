b8879aee8da1a70734b4e127ece8e00894ad07f3
Brandon Bartram <brandon@immersivevr.co.uk>
2023-01-24 21:09:18 +0000

    first commit

---
+++ src/pages/api/generate.js
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
+const basePromptPrefix = `
+Talk to me like you are the character 'The Dude' from the film 'The Big Lebowski'
+`
+const generateAction = async (req, res) => {
+	// Run first prompt
+	console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+	const baseCompletion = await openai.createCompletion({
+		model: 'text-davinci-003',
+		prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+		temperature: 0.7,
+		max_tokens: 150,
+	})
+
+	const basePromptOutput = baseCompletion.data.choices.pop()
+
+	res.status(200).json({ output: basePromptOutput })
+}
+
+export default generateAction
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				manrope: ['Manrope', 'sans-serif'],
+			},
+			colors: {
+				lebowski: '#f9d08e',
+			},
+			backgroundImage: {
+				dude: "url('/thedude.jpeg')",
+			},
+		},
+	},
+	plugins: [],
+}
