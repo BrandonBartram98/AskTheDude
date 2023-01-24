474451a6a1122d833cf6eec08707b77901d04760
Brandon Bartram <brandon@immersivevr.co.uk>
2023-01-24 21:12:43 +0000

    fixed noreferrer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -98,16 +98,21 @@ export default function Home() {
 				<div className="flex gap-4 text-left justify-between mt-8 items-center">
 					<span className="text-lebowski text-xs">
 						Powered by&nbsp;
-						<a target="_blank" href="https://openai.com/" className="hover:text-lebowski/60">
+						<a target="_blank" rel="noreferrer" href="https://openai.com/" className="hover:text-lebowski/60">
 							<span className="underline">OpenAI,</span>&nbsp;
 						</a>
-						<a target="_blank" href="https://vercel.com/" className="hover:text-lebowski/60">
+						<a target="_blank" rel="noreferrer" href="https://vercel.com/" className="hover:text-lebowski/60">
 							<span className="underline">Vercel</span>&nbsp;
 						</a>
 						& The Dude&#8482;
 					</span>
 					<div className="flex gap-3 justify-end">
-						<a target="_blank" href="https://github.com/BrandonBartram98" className="w-6 h-6 fill-white hover:fill-white/50 transition-all">
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://github.com/BrandonBartram98"
+							className="w-6 h-6 fill-white hover:fill-white/50 transition-all"
+						>
 							<svg viewBox="0 0 128 128">
 								<g>
 									<path
@@ -119,12 +124,22 @@ export default function Home() {
 								</g>
 							</svg>
 						</a>
-						<a target="_blank" href="https://www.linkedin.com/in/brandonbartram/" className="w-6 h-6 fill-white hover:fill-white/50 transition-all">
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://www.linkedin.com/in/brandonbartram/"
+							className="w-6 h-6 fill-white hover:fill-white/50 transition-all"
+						>
 							<svg viewBox="0 0 128 128">
 								<path d="M116 3H12a8.91 8.91 0 00-9 8.8v104.42a8.91 8.91 0 009 8.78h104a8.93 8.93 0 009-8.81V11.77A8.93 8.93 0 00116 3zM39.17 107H21.06V48.73h18.11zm-9-66.21a10.5 10.5 0 1110.49-10.5 10.5 10.5 0 01-10.54 10.48zM107 107H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87 7.36-10.87 15V107H50.53V48.73h17.36v8h.24c2.42-4.58 8.32-9.41 17.13-9.41C103.6 47.28 107 59.35 107 75z"></path>
 							</svg>
 						</a>
-						<a target="_blank" href="https://twitter.com/brandon_bartram" className="w-6 h-6 fill-white hover:fill-white/50 transition-all">
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://twitter.com/brandon_bartram"
+							className="w-6 h-6 fill-white hover:fill-white/50 transition-all"
+						>
 							<svg viewBox="0 0 128 128">
 								<path d="M40.254 127.637c48.305 0 74.719-48.957 74.719-91.403 0-1.39 0-2.777-.075-4.156 5.141-4.547 9.579-10.18 13.102-16.633-4.79 2.602-9.871 4.305-15.078 5.063 5.48-4.02 9.582-10.336 11.539-17.774-5.156 3.743-10.797 6.38-16.68 7.801-8.136-10.586-21.07-13.18-31.547-6.32-10.472 6.86-15.882 21.46-13.199 35.617C41.922 38.539 22.246 26.336 8.915 6.27 1.933 20.94 5.487 39.723 17.022 49.16c-4.148-.172-8.207-1.555-11.832-4.031v.41c0 15.273 8.786 28.438 21.02 31.492a21.596 21.596 0 01-11.863.543c3.437 13.094 13.297 22.07 24.535 22.328-9.305 8.918-20.793 13.75-32.617 13.72-2.094 0-4.188-.15-6.266-.446 12.008 9.433 25.98 14.441 40.254 14.422"></path>
 							</svg>
